3e3f5cd5dc0850b82951a6586565092cbb548fd1
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-09-16 18:41:43 +0100

    add AbstractEntity class with timestamp fields for entity tracking

---
+++ app/src/main/java/org/example/entities/AbstractEntity.java
@@ -0,0 +1,39 @@
+package org.example.entities;
+
+import java.time.LocalDateTime;
+
+public abstract class AbstractEntity<ID> {
+
+    protected LocalDateTime createdAt;
+    protected LocalDateTime updatedAt;
+    protected LocalDateTime deletedAt;
+
+    public abstract ID getId();
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public AbstractEntity setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public AbstractEntity setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+        return this;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    public AbstractEntity setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+        return this;
+    }
+}
